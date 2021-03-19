5cec887b6c39db8a7173d72b3e5096cf1386fc50
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-19 00:32:01 -0300

    update exerc

---
+++ programas C/encontros__/Exercicios/Exerc02/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+float mediaDoAluno(float nota1, float nota2){
+	float media;
+	media = nota1 + nota2;
+	
+	return (media);
+}
+int main(int argc, char *argv[]) {
+	float nota1, nota2, resultado;
+	
+	printf("Informe o valor da nota1: ");
+	scanf("%f",&nota1);
+	printf("Informe o valor da nota2: ");
+	scanf("%f",&nota2);
+	
+	resultado = mediaDoAluno(nota1, nota2);
+	printf("Soma: %.2f", resultado);
+	return 0;
+}
