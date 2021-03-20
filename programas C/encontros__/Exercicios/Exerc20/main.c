90377dad583c1e0f75ea39ff805dfa0f112ec7b1
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-20 02:47:14 -0300

    update exerc

--- programas C/encontros__/Exercicios/Exerc20/main.c
+++ programas C/encontros__/Exercicios/Exerc20/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* Quando o número de linhas é igual ao número de colunas a matriz é dita quadrada. Neste
+caso, os elementos de índices iguais constituem a diagonal principal. A soma dos elementos
+da diagonal principal de uma matriz quadrada é chamada de traço da matriz. Elabore um
+programa que determine o traço de uma matriz quadrada de ordem n dada, com n menor
+ou igual a 30. */
 
 int main(int argc, char *argv[]) {
+	int ordem_n, i, j;
+	
+	printf("informe a ordem da matriz: ");
+	scanf("%d",&ordem_n);
+	
+	int matriz[ordem_n][ordem_n];
+	
+	for(i = 0; i < ordem_n; i++){
+		for(j = 0; j < ordem_n; j++){
+			printf("informe o valor de %.%: ", i+1,j+1);
+			scanf("%d",&matriz[i][j]);
+		}
+	}
+	
 	return 0;
 }
