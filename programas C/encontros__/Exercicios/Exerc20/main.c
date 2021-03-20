938f0161cc9b2b61632998892593e0cab8904040
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-20 02:54:06 -0300

    update exerc20

--- programas C/encontros__/Exercicios/Exerc20/main.c
+++ programas C/encontros__/Exercicios/Exerc20/main.c
@@ -8,19 +8,44 @@ programa que determine o traço de uma matriz quadrada de ordem n dada, com n men
 ou igual a 30. */
 
 int main(int argc, char *argv[]) {
-	int ordem_n, i, j;
+	int ordem_n, i, j, soma;
 	
 	printf("informe a ordem da matriz: ");
 	scanf("%d",&ordem_n);
 	
 	int matriz[ordem_n][ordem_n];
 	
+	// prienchendo a matriz
 	for(i = 0; i < ordem_n; i++){
 		for(j = 0; j < ordem_n; j++){
-			printf("informe o valor de %.%: ", i+1,j+1);
+			printf("informe o valor de %d.%d: ", i+1,j+1);
 			scanf("%d",&matriz[i][j]);
 		}
 	}
+	
+	// somando os valores da diagonal da matriz
+	soma = 0;
+	
+	for (i = 0; i < ordem_n; i++){
+		for(j = 0; j < ordem_n; j++){
+			if(i==j){
+				soma = soma + matriz[i][j];
+			}
+		}
+	}
+	
+	// imprimindo a matriz
+	for (i = 0; i < ordem_n; i++){
+		for(j = 0; j < ordem_n;j++){
+			printf("%d ",matriz[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\n");
+	
+	printf("A soma dos valores da diagonal h: %d", soma);
+	
 	
 	return 0;
 }
