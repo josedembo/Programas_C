2c6c02560061358cc9087499912a4a307a73e9fb
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-21 20:34:08 -0300

    update Exerc08

---
+++ programas C/encontros__/Exercicios/Exerc08/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Elabore uma função que receba 2 (dois) inteiros positivos,
+ calcule e retorne o quociente da divisão inteira do primeiro pelo segundo. 
+Elabore também um programa principal para mostrar no vídeo o retorno da função. */
+
+int divisaoInteira(int numero1, int numero2){
+	int quociente;
+	quociente = numero1 / numero2 ;
+	return quociente;
+}
+
+
+int main(int argc, char *argv[]) {
+	int n1, n2, result;
+	
+	printf("informe o primeiro numero inteiro: ");
+	scanf("%d",&n1);
+	
+	printf("informe o segundo numero inteiro: ");
+	scanf("%d",&n2);
+	
+	result = divisaoInteira(n1, n2);
+	
+	printf("A divisao inteira entre %d e %d he: %d \n", n1, n2,result);
+	return 0;
+}
