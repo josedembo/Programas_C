d518b295e71cdd402662f460aa6341491dbe94b3
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-23 17:50:35 -0300

    update Exerc14

--- programas C/encontros__/Exercicios/Exerc14/main.c
+++ programas C/encontros__/Exercicios/Exerc14/main.c
@@ -6,33 +6,54 @@ se o vetor dado for (2, 5, 7, 10, 12, 13) e o elemento a ser removido for o de í
 o algoritmo deve fornecer o vetor (2, 5, 7, 12, 13).*/
 
 void removaElemento(int vetor[], int indice, int tamanho){
-	int i, j;
+	int i, j, novo_vetor[tamanho-1], cont;
+	
+	cont = 0;
 	
 	for (i = 0; i<tamanho; i++){
-		if (i >= indice){
-			vetor[i] = vetor[i+1];
+		if (i == indice){
+			continue;
 		}else{
-			vetor[i] = vetor[i];
+			novo_vetor[cont] = vetor[i];
 		}
+		cont += 1;
 	}
 	
 	for(j = 0; j < tamanho - 1; j++){
-		printf("%d ", vetor[j]);
+		printf("%d ", novo_vetor[j]);
 	
 	}
+	
 	
 }
 
 
 int main(int argc, char *argv[]) {
-/*
-	int vetor[6] = {2, 5, 7, 10, 12, 13};
-	int i;
-	for(i = 0; i<6;i++){
-		printf("%d",vetor[i]);
+	int i, tam, ind;
+	printf("digite o tamanho do vetor: \n");
+	scanf("%d", &tam);
+	
+	int vet[tam];
+	int cont_i = 0;
+	
+	while(cont_i<tam){
+	printf("Digite o proximo  elemento %d do vetor: ", i+1);
+	scanf("%d",&vet[cont_i]);
+	cont_i+=1;
 	}
-*/
-	int vetor[6] = {2, 5, 7, 10, 12, 13};
-	removaElemento(vetor,0,6);
+
+
+	// aqui mostra o vetor como está
+	printf("Vetor actual\n");
+	for ( i = 0; i < tam; i++  ){
+		printf("%d ", vet[i]);
+	}
+	
+	printf("\nDigite o indice que deseja apagar: \n");
+	scanf("%d", &ind);
+	
+	printf("Vetor alterado\n");
+	removaElemento(vet, ind, tam);
+
 	return 0;
 }
